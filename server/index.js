f621b8a7bb6f7600687914e2a496ddd3ab3b6a0e
lapboi3 <74074061+ItsLap@users.noreply.github.com>
2021-05-09 15:43:01 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.render("Home", {
+        hgb1: "Easy to use",
+        hgb2: "User friendly",
+        hgb3: "Free forever"
+    })
+})
+
+app.get("/app", (req, res) => {
+    res.render("App", {})
+})
+
+io.on('connection', (socket) => {
+    socket.on("update-text", (txt) => {
+        io.emit("update-text", (txt))
+    })
+})
+
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
+});
